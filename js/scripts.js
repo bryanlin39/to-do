f544f089cc6b4ec02ae0be6a8f5edd36a00026c2
Bryan Lin and Ben Metzger <ben@>
2017-04-03 16:26:33 -0700

    fix bug with checkboxes to fully remove p element parents of the checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Make a to do list webpage where people can add tasks to create a list of things to do. Add an option for users to indicate a task is done. Use jQuery to remove the task from the "to do" list. Make sure to identify specific behaviors your program should demonstrate, and create specifications with example input and output. List these in the project's README.
-function Task(taskName, priority, notes, dueDate, checkBox) {
+function Task(taskName, priority, notes, dueDate) {
   this.taskName = taskName;
   this.priority = priority;
   this.notes = notes;
   this.dueDate = dueDate;
-  this.checkBox = checkBox;
+  // this.checkBox = checkBox;
 }
 
 $(document).ready(function() {
@@ -15,22 +15,19 @@ $(document).ready(function() {
     var priority = $("#priority").val();
     var notes = $("#notes").val();
     var dueDate = $("#due").val();
-    var checkBox = "<input type='checkbox' name='checkBox'>"
 
+    var newTask = new Task(taskName, priority, notes, dueDate);
 
-    var newTask = new Task(taskName, priority, notes, dueDate, checkBox);
-
-    $("#task-list").append("<p>" + newTask.checkBox + "<span class='task'>" + newTask.taskName + "</span></p>");
+    $("#task-list").append("<p><input type='checkbox' name='checkBox'><span class='task'>" + newTask.taskName + "</span></p>");
 
 
     $("input[name='checkBox']").click(function() {
       if (this.checked) {
-        $(this).next().remove();
-        $(this).remove();
+        $(this).parent().remove()
       }
     });
 
-    $(".task").click(function() {
+    $(".task").last().click(function() {
       $("#show-task").toggle();
       $("#show-task h2").text(newTask.taskName);
       $("#priority-output").text(newTask.priority);
